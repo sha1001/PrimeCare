9e177cb393243c28ad71f3f675f18137e642c0df
Nitin Sharma <nitin.sharma@blackboard.com>
2018-03-20 16:13:30 -0700

    Fixed login

--- src/app/PrimeCareManager/component/sidenav/sidenav.component.ts
+++ src/app/PrimeCareManager/component/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import { MatSidenav } from '@angular/material';
@@ -10,7 +10,10 @@ const SMALL_WIDTH_BREAKPOINT = 720;
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, AfterViewInit {
+
+  sidetop = 0;
+  sidemarginbottom = 0;
 
   private mediaMatcher: MediaQueryList =
     matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
@@ -23,6 +26,7 @@ export class SidenavComponent implements OnInit {
     public router: Router) {
     this.mediaMatcher.addListener(mql =>
       zone.run(() => this.mediaMatcher = mql));
+
   }
 
   @ViewChild(MatSidenav) sidenav: MatSidenav;
@@ -30,11 +34,34 @@ export class SidenavComponent implements OnInit {
 
   ngOnInit() {
 
+    console.log('Nitin');
+
     this.router.events.subscribe(() => {
-      if (this.isScreenSmall()) {
-        this.sidenav.close();
-      }
+      console.log('Nitin2');
+      if (this.router.url.includes('signin')) {
+        this.sidetop = 0;
+        this.sidemarginbottom = 0;
+        console.log('sidetop1');
+       } else {
+        this.sidetop = 200;
+        this.sidemarginbottom = 50;
+        console.log('sidetop2');
+       }
     });
+   
+
+  }
+
+  ngAfterViewInit() {
+    if (this.router.url.includes('signin')) {
+      this.sidetop = 0;
+      this.sidemarginbottom = 0;
+      console.log('sidetop1');
+     } else {
+      this.sidetop = 200;
+      this.sidemarginbottom = 50;
+      console.log('sidetop2');
+     }
   }
 
   isScreenSmall(): boolean {
